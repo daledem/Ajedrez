9bdd293848470928d3153c14d23f6484b9ce42b2
daledem <davidledesma973@gmail.com>
2023-11-25 19:16:15 +0100

    Estructura del servidor creado

---
+++ src/main/java/org/example/Servidor.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Servidor {
+
+    public static void main(String[] args) {
+        Socket s = null;
+        ExecutorService pool = Executors.newCachedThreadPool();
+
+        try (ServerSocket ss = new ServerSocket(55555)){
+            while (true){
+                try{
+                    s = ss.accept();
+
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                if (s != null) {
+                    s.close();
+                }
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+            pool.shutdown();
+        }
+    }
+
+}
