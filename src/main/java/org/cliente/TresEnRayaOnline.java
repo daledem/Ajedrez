bce5e083b06c9f6b1cb66d046fbbd336a0d52959
daledem <davidledesma973@gmail.com>
2023-12-10 19:37:26 +0100

    GUI convertida a codigo java

--- src/main/java/org/cliente/TresEnRayaOnline.java
+++ src/main/java/org/cliente/TresEnRayaOnline.java
@@ -3,6 +3,7 @@ package org.cliente;
 import org.TresEnRaya.Game;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
@@ -32,7 +33,7 @@ public class TresEnRayaOnline {
     private JButton button23;
     private JButton button33;
 
-    public TresEnRayaOnline(Socket s, ServerSocket ss, Game game, String nombre, String contrasena){
+    public TresEnRayaOnline(Socket s, ServerSocket ss, Game game, String nombre, String contrasena) {
         this.s = s;
         this.ss = ss;
         this.game = game;
@@ -51,7 +52,7 @@ public class TresEnRayaOnline {
             @Override
             public void windowClosed(WindowEvent e) {
                 try {
-                    if (oos != null){
+                    if (oos != null) {
                         oos.close();
                     }
                 } catch (IOException ex) {
@@ -59,7 +60,7 @@ public class TresEnRayaOnline {
                 }
 
                 try {
-                    if(ois != null){
+                    if (ois != null) {
                         ois.close();
                     }
                 } catch (IOException ex) {
@@ -67,7 +68,7 @@ public class TresEnRayaOnline {
                 }
 
                 try {
-                    if(s != null){
+                    if (s != null) {
                         s.close();
                     }
                 } catch (IOException ex) {
@@ -75,7 +76,7 @@ public class TresEnRayaOnline {
                 }
 
                 try {
-                    if (ss != null){
+                    if (ss != null) {
                         ss.close();
                     }
                 } catch (IOException ex) {
@@ -93,7 +94,7 @@ public class TresEnRayaOnline {
                 this.game = (Game) ois.readObject();
                 this.activarBotones();
                 this.actualizarBotones();
-            }else {
+            } else {
                 this.turno = "X";
             }
         } catch (IOException e) {
@@ -104,7 +105,7 @@ public class TresEnRayaOnline {
         button11.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if(TresEnRayaOnline.this.button11.isEnabled()) {
+                if (TresEnRayaOnline.this.button11.isEnabled()) {
                     TresEnRayaOnline.this.gestionarBoton(1, 1);
                 }
             }
@@ -113,7 +114,7 @@ public class TresEnRayaOnline {
         button12.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if(TresEnRayaOnline.this.button12.isEnabled()) {
+                if (TresEnRayaOnline.this.button12.isEnabled()) {
                     TresEnRayaOnline.this.gestionarBoton(1, 2);
                 }
             }
@@ -122,7 +123,7 @@ public class TresEnRayaOnline {
         button13.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if(TresEnRayaOnline.this.button13.isEnabled()) {
+                if (TresEnRayaOnline.this.button13.isEnabled()) {
                     TresEnRayaOnline.this.gestionarBoton(1, 3);
                 }
             }
@@ -131,7 +132,7 @@ public class TresEnRayaOnline {
         button21.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if(TresEnRayaOnline.this.button21.isEnabled()) {
+                if (TresEnRayaOnline.this.button21.isEnabled()) {
                     TresEnRayaOnline.this.gestionarBoton(2, 1);
                 }
             }
@@ -140,7 +141,7 @@ public class TresEnRayaOnline {
         button22.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if(TresEnRayaOnline.this.button22.isEnabled()) {
+                if (TresEnRayaOnline.this.button22.isEnabled()) {
                     TresEnRayaOnline.this.gestionarBoton(2, 2);
                 }
             }
@@ -149,7 +150,7 @@ public class TresEnRayaOnline {
         button23.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if(TresEnRayaOnline.this.button23.isEnabled()) {
+                if (TresEnRayaOnline.this.button23.isEnabled()) {
                     TresEnRayaOnline.this.gestionarBoton(2, 3);
                 }
             }
@@ -158,7 +159,7 @@ public class TresEnRayaOnline {
         button31.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if(TresEnRayaOnline.this.button31.isEnabled()) {
+                if (TresEnRayaOnline.this.button31.isEnabled()) {
                     TresEnRayaOnline.this.gestionarBoton(3, 1);
                 }
             }
@@ -167,7 +168,7 @@ public class TresEnRayaOnline {
         button32.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if(TresEnRayaOnline.this.button32.isEnabled()) {
+                if (TresEnRayaOnline.this.button32.isEnabled()) {
                     TresEnRayaOnline.this.gestionarBoton(3, 2);
                 }
             }
@@ -176,27 +177,27 @@ public class TresEnRayaOnline {
         button33.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if(TresEnRayaOnline.this.button33.isEnabled()) {
+                if (TresEnRayaOnline.this.button33.isEnabled()) {
                     TresEnRayaOnline.this.gestionarBoton(3, 3);
                 }
             }
         });
     }
 
-    public void showInterface(){
+    public void showInterface() {
         frame.setContentPane(this.panel1);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
     }
 
-    public void gestionarBoton(int colum, int row){
+    public void gestionarBoton(int colum, int row) {
         try {
             game.changeTurn();
-            if (game.markSquare(colum - 1,row - 1)){
+            if (game.markSquare(colum - 1, row - 1)) {
                 oos.writeObject(game);
                 oos.flush();
-                if(game.finish()){
+                if (game.finish()) {
                     terminarJuego();
                     return;
                 }
@@ -204,13 +205,13 @@ public class TresEnRayaOnline {
                 this.game = (Game) ois.readObject();
                 this.activarBotones();
                 this.actualizarBotones();
-                if(game.finish()){
+                if (game.finish()) {
                     terminarJuego();
                 }
-            }else {
+            } else {
                 game.changeTurn();
             }
-        } catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
@@ -218,7 +219,7 @@ public class TresEnRayaOnline {
 
     }
 
-    public void actualizarBotones(){
+    public void actualizarBotones() {
         this.button11.setText(game.getTable()[0][0].getType());
         this.button12.setText(game.getTable()[0][1].getType());
         this.button13.setText(game.getTable()[0][2].getType());
@@ -230,8 +231,8 @@ public class TresEnRayaOnline {
         this.button33.setText(game.getTable()[2][2].getType());
         this.frame.pack();
     }
-    
-    public void activarBotones(){
+
+    public void activarBotones() {
         this.button11.setEnabled(true);
         this.button12.setEnabled(true);
         this.button13.setEnabled(true);
@@ -243,7 +244,7 @@ public class TresEnRayaOnline {
         this.button33.setEnabled(true);
     }
 
-    public void desactivarBotones(){
+    public void desactivarBotones() {
         this.button11.setEnabled(false);
         this.button12.setEnabled(false);
         this.button13.setEnabled(false);
@@ -256,7 +257,7 @@ public class TresEnRayaOnline {
     }
 
     public void terminarJuego() {
-        try (Socket s = new Socket("192.168.56.1", 55555);PrintStream ps = new PrintStream(s.getOutputStream())){
+        try (Socket s = new Socket("192.168.56.1", 55555); PrintStream ps = new PrintStream(s.getOutputStream())) {
             int puntos;
 
             if (this.game.getWinner().equals(" ")) {
@@ -276,22 +277,75 @@ public class TresEnRayaOnline {
 
             ps2.println("GET " + nombre + " " + contrasena);
             ps2.flush();
-            if (br2.readLine().equals("OK")){
-                Lobby lobby = new Lobby(nombre,contrasena,br2,ps2);
+            if (br2.readLine().equals("OK")) {
+                Lobby lobby = new Lobby(nombre, contrasena, br2, ps2);
                 lobby.showInterface();
                 this.frame.dispose();
-            }else {
+            } else {
                 try {
-                    if(s2 != null){
+                    if (s2 != null) {
                         s2.close();
                     }
-                } catch (IOException e){
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
 
-        } catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    {
+// GUI initializer generated by IntelliJ IDEA GUI Designer
+// >>> IMPORTANT!! <<<
+// DO NOT EDIT OR ADD ANY CODE HERE!
+        $$$setupUI$$$();
+    }
+
+    /**
+     * Method generated by IntelliJ IDEA GUI Designer
+     * >>> IMPORTANT!! <<<
+     * DO NOT edit this method OR call it in your code!
+     *
+     * @noinspection ALL
+     */
+    private void $$$setupUI$$$() {
+        panel1 = new JPanel();
+        panel1.setLayout(new com.intellij.uiDesigner.core.GridLayoutManager(3, 3, new Insets(10, 10, 10, 10), -1, -1));
+        button11 = new JButton();
+        button11.setText(" ");
+        panel1.add(button11, new com.intellij.uiDesigner.core.GridConstraints(0, 0, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        button21 = new JButton();
+        button21.setText(" ");
+        panel1.add(button21, new com.intellij.uiDesigner.core.GridConstraints(0, 1, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        button22 = new JButton();
+        button22.setText(" ");
+        panel1.add(button22, new com.intellij.uiDesigner.core.GridConstraints(1, 1, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        button32 = new JButton();
+        button32.setText(" ");
+        panel1.add(button32, new com.intellij.uiDesigner.core.GridConstraints(1, 2, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        button12 = new JButton();
+        button12.setText(" ");
+        panel1.add(button12, new com.intellij.uiDesigner.core.GridConstraints(1, 0, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        button13 = new JButton();
+        button13.setText(" ");
+        panel1.add(button13, new com.intellij.uiDesigner.core.GridConstraints(2, 0, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        button23 = new JButton();
+        button23.setText(" ");
+        panel1.add(button23, new com.intellij.uiDesigner.core.GridConstraints(2, 1, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        button33 = new JButton();
+        button33.setText(" ");
+        panel1.add(button33, new com.intellij.uiDesigner.core.GridConstraints(2, 2, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        button31 = new JButton();
+        button31.setText(" ");
+        panel1.add(button31, new com.intellij.uiDesigner.core.GridConstraints(0, 2, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+    }
+
+    /**
+     * @noinspection ALL
+     */
+    public JComponent $$$getRootComponent$$$() {
+        return panel1;
     }
 }
