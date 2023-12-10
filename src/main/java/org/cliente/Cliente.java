c1448a96786b257dc3d0877e95d4e32ab1b1d245
daledem <davidledesma973@gmail.com>
2023-12-10 20:47:34 +0100

    Intento fallido de solucion de problemas de la interfaz grafica fuera de intelij

--- src/main/java/org/cliente/Cliente.java
+++ src/main/java/org/cliente/Cliente.java
@@ -1,18 +1,5 @@
 package org.cliente;
 
-import org.TresEnRaya.Game;
-
-import javax.swing.*;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintStream;
-import java.net.InetAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.net.UnknownHostException;
-import java.util.Scanner;
-
 public class Cliente {
 
     public static void main(String[] args) {
